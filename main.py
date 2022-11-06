7ee1c42c31e451683432ffb48c9d517c47646728
Leaves <381818896@qq.com>
2022-11-06 15:45:47 +0800

    add

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 from src.cmpTree import *
 from src.functions import *
 from src.formulaGraph import *
-test = [
-    f_add, f_minus, f_norm, 1, f_norm, 1
-]
-
-test2 = [
-    f_implies, is_closed
-]
-
-for e in fgraph.search(test):
-    print(e)
+# test = [
+#     f_add, f_minus, f_norm, 1, f_norm, 1
+# ]
+#
+#
+# for e in fgraph.search(test):
+#     print(e)
 
 print()
+
+
+test2 = theorem_query(
+    [is_compact, is_sequence],
+    []
+)
+
 
 for e in tgraph.search(test2):
     print(e)
--- src/formulaGraph.py
+++ src/formulaGraph.py
@@ -22,10 +22,29 @@ class fGraph:
             self.table[v.id] = v
 
     def search(self, formula: list):
-        result = []
+        score_index = 2
+        if formula[0] == f_implies:
+            f1 = formula
+            f2 = [f_implies, (f_and, 1)] + f1[1:]
+            r1, r2 = self._search(f1), self._search(f2)
+            for v in r1:
+                r1[v][score_index] = max(r1[v][score_index], r2[v][score_index])
+            result = r1
+        else:
+            result = self._search(formula)
+        return sorted(list(result.values()), key=lambda x: x[score_index], reverse=True)
+
+    def _search(self, formula: list):
+        result = {}
         for v in self.vertexs:
-            result.append(tuple([v.id, v.name, cmp_tree(v.content, formula), v.comment]))
-        return sorted(result, key=lambda x: x[2], reverse=True)
+            result[v.id] = [v.id, v.name, cmp_tree(formula, v.content), v.comment] # score_index = 2
+        return result
+
+    # def _search(self, formula: list):
+    #     result = []
+    #     for v in self.vertexs:
+    #         result.append(tuple([v.id, v.name, cmp_tree(v.content, formula), v.comment]))
+    #     return sorted(result, key=lambda x: x[2], reverse=True)
 
 
 fgraph = fGraph([
@@ -54,12 +73,22 @@ fgraph = fGraph([
     )
 ])
 
-def theorem_generator(conditon: list, conlusion:list):
-    if not conditon[0].startswith("is_"):
+def theorem_query(conditon: list[str | tuple], conlusion:list[str | tuple]):
+    if len(conditon) == 0:
+        conditon = [is_trivial]
+    if len(conlusion) == 0:
+        conlusion = [is_trivial]
+    return theorem_generator(conditon, conlusion)
+
+def theorem_generator(conditon: list[str | tuple], conlusion:list[str | tuple]):
+    assert type(conditon[0]) == str or type(conditon[0]) == tuple
+    assert conditon.count(f_and) <= 1
+    assert conlusion.count(f_and) <= 1
+    if type(conditon[0]) == tuple or not conditon[0].startswith("is_"):
         imply = [(f_implies, 2)]
     else:
         imply = [(f_implies, len(conditon) + 1)]
-    if not conlusion[0].startswith("is_"):
+    if type(conlusion[0]) == tuple or not conlusion[0].startswith("is_"):
         answer = [(f_answer, 1)]
     else:
         answer = [(f_answer, len(conlusion))]
@@ -158,7 +187,98 @@ tgraph = fGraph([
         ),
         "Bady/Rudin/E13/TM2.27"
     ),
-
+    fNode(
+        12,
+        "If F is a closed set, and E is a subset of F, then the closure(E) is a subset of F",
+        theorem_generator(
+            [is_subset, is_closed],
+            [is_subset, is_closure]
+        ),
+        "Bady/Rudin/E13/TM2.27"
+    ),
+    fNode(
+        13,
+        "If y = sup E and if E is closed, then y is in E",
+        theorem_generator(
+            [(f_and, 2), f_equal, 1, is_sup, is_closed],
+            [f_is_in, 1, is_set]
+        ),
+        "Bady/Rudin/E13/TM2.28"
+    ),
+    fNode(
+        14,
+        "Y \subset X; E \subset Y; then E is open relative to Y iff E = Y intersection G for some open open subset G of X",
+        theorem_generator(
+            [(f_and, 2), is_subset, is_open_relative_to],
+            [f_equal, is_set, (f_and, 2), is_intersection, is_open]
+        ),
+        "Bady/Rudin/E13/TM2.30"
+    ),
+    fNode(
+        15,
+        "E = Y intersection G for some open open subset G of X iff Y \subset X; E \subset Y; then E is open relative to Y ",
+        theorem_generator(
+            [f_equal, is_set, (f_and, 2), is_intersection, is_open],
+            [(f_and, 2), is_subset, is_open_relative_to]
+        ),
+        "Bady/Rudin/E13/TM2.30"
+    ),
+    fNode(
+        16,
+        "Suppose K \subset Y \subset X. Then K is compact relative to X iff K is compact relative to Y",
+        theorem_generator(
+            [is_subset, is_subset],
+            [is_compact_relative_to, is_compact_relative_to]
+        ),
+        "Bady/Rudin/E13/TM2.33"
+    ),
+    fNode(
+        17,
+        "Compact subsets of metric spaces are closed",
+        theorem_generator(
+            [is_compact, is_metric_space],
+            [is_closed]
+        ),
+        "Bady/Rudin/E13/TM2.34"
+    ),
+    fNode(
+        18,
+        "Closed subsets of compact sets are compact",
+        theorem_generator(
+            [is_closed, is_subset, is_compact],
+            [is_compact]
+        ),
+        "Bady/Rudin/E13/TM2.35"
+    ),
+    fNode(
+        19,
+        "If F is closed and K is compact, then F \intersection K is compact",
+        theorem_generator(
+            [is_closed, is_compact],
+            [is_compact, is_intersection]
+        ),
+        "Bady/Rudin/E13/TM2.35"
+    ),
+    fNode(
+        20,
+        "If {K_\alpha} is a collection of compact subsets of a metric space X such that the intersection of every finite subcollection of {K_\alpha} is nonempty, then \intersection K_\alpha is nonempty",
+        theorem_generator(
+            [(f_and, 6),
+             is_set, is_compact, is_metric_space,
+             is_intersection, is_finite, is_not_empty],
+            [is_intersection, is_not_empty]
+        ),
+        "Bady/Rudin/E13/TM2.36"
+    ),
+    fNode(
+        21,
+        "If {K_n} is a sequence of nonempty compact sets such that K_n \supset K_{n+1}, then \cap K_n is not empty",
+        theorem_generator(
+            [is_sequence, is_not_empty, is_compact, is_subset],
+            [is_intersection, is_not_empty]
+        ),
+        "Bady/Rudin/E13/TM2.36"
+    )
 
 ])
 
